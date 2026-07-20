493948bd1f9bcc824df091ca36bfcce805e13351
okivictor01-bit <okivictor01@gmail.com>
2026-07-20 14:37:59 +0100

    Add files via upload

--- app/staff/layout.tsx
+++ app/staff/layout.tsx
@@ -1,1 +1,5 @@
-hdhjdj
+import SubscriptionGate from "@/components/SubscriptionGate";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <SubscriptionGate>{children}</SubscriptionGate>;
+}
